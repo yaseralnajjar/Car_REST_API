e25fb060f583ecfe795ba4ccac4ebdfb8b85933d
YaserAlnajjar <kasseryasser@windowslive.com>
2022-02-12 14:12:11 +0300

    Add auth jwt urls

--- cars/urls.py
+++ cars/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 from cars import views
 
 app_name = "cars"
@@ -9,6 +10,9 @@ urlpatterns = [
     path("cars/<int:pk>/", views.CarDetail.as_view(), name="car-detail"),
     path("users/", views.UsersList.as_view(), name="users-list"),
     path("users/<int:pk>/", views.UserDetail.as_view(), name="user-detail"),
+    path('auth/obtain-jwt-token/', obtain_jwt_token, name="obtain-jwt-token"),
+    path('auth/refresh-token/', refresh_jwt_token, name="refresh-token"),
+    path('auth/verify-token/', verify_jwt_token, name="verify-token"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
